c88f550d36854cacf786704d5a4516a3f6cc4c4d
Evan Shapiro <evan.a.shapiro@gmail.com>
2012-11-10 16:42:30 -0500

    added console logs -> toast

--- CordovaBrowser/src/com/eas/cordova/browser/CordovaBrowserActivity.java
+++ CordovaBrowser/src/com/eas/cordova/browser/CordovaBrowserActivity.java
@@ -1,32 +1,33 @@
 package com.eas.cordova.browser;
 
-import android.app.Activity;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.widget.ProgressBar;
+import android.webkit.ConsoleMessage;
+import android.widget.Toast;
 
+import org.apache.cordova.CordovaChromeClient;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaWebViewClient;
 import org.apache.cordova.DroidGap;
 
 public class CordovaBrowserActivity extends DroidGap {
 	/** Called when the activity is first created. */
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		String defaultUrl = "file:///android_asset/www/index.html";
 		String url = getUrlFromIntent(getIntent(), defaultUrl);
 		super.loadUrl(url);
 		super.appView.clearCache(false);
-		
+
 	}
-	
+
 
 	public String getUrlFromIntent(Intent i, String defaultUrl){
 		String webURI = i.getStringExtra(Intent.EXTRA_TEXT);
@@ -42,7 +43,7 @@ public class CordovaBrowserActivity extends DroidGap {
 		inflater.inflate(R.menu.menu, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (item.getItemId() == R.id.home){
@@ -52,6 +53,38 @@ public class CordovaBrowserActivity extends DroidGap {
 			super.appView.clearCache(false);
 			super.appView.reload();
 		}
-	    return true;
+		return true;
+	}
+
+	private void toastConsoleMessage(String message, int lineNumber, String sourceID){
+		if (message != null && sourceID != null &&
+				!message.contains("JSCallback Error: Request failed with status 0")){
+			Toast.makeText(this,
+					sourceID + ": Line " + lineNumber + " : " + message, 
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	@Override
+	public void init() {
+		CordovaWebView webView = new CordovaWebView(this);
+		CordovaWebViewClient webViewClient;
+		webViewClient = new CordovaWebViewClient(this, webView);
+		this.init(webView, webViewClient, new CordovaChromeClient(this, webView){
+			@Override
+			public void onConsoleMessage(String message, int lineNumber, String sourceID){
+				toastConsoleMessage(message, lineNumber, sourceID);
+				super.onConsoleMessage(message, lineNumber, sourceID);
+			}
+
+			@TargetApi(8)
+			@Override
+			public boolean onConsoleMessage(ConsoleMessage consoleMessage){
+				toastConsoleMessage(consoleMessage.message(),
+						consoleMessage.lineNumber(),
+						consoleMessage.sourceId());
+				return super.onConsoleMessage(consoleMessage);
+			}
+		});
 	}
 }
