2c80b800b5236e4aaf2b521c6836a142a282c801
Evan Shapiro <evan.a.shapiro@gmail.com>
2012-11-10 18:23:05 -0500

    added check for replace state support

--- CordovaBrowser/assets/www/js/BrowserHistoryManager.js
+++ CordovaBrowser/assets/www/js/BrowserHistoryManager.js
@@ -1,31 +1,37 @@
 var BrowserHistoryManager = function(loaderApp){
-    window.onpopstate = function(event) {
-        if (event.state.toRevert === 'hideCreateUrlDialog'){
-            loaderApp.createUrlDialog.hideCreateUrlDialog();
-        }
-        else if (event.state.toRevert === 'hideUrlSettingsDialog'){
-            loaderApp.urlSettingsDialog.hideUrlSettingsDialog();
-        }
-        else if (event.state.toRevert === 'slideFromSettingsView'){
-            loaderApp.settingsView.cancel();
-        }
-    }
+	window.onpopstate = function(event) {
+		if (event.state.toRevert === 'hideCreateUrlDialog'){
+			loaderApp.createUrlDialog.hideCreateUrlDialog();
+		}
+		else if (event.state.toRevert === 'hideUrlSettingsDialog'){
+			loaderApp.urlSettingsDialog.hideUrlSettingsDialog();
+		}
+		else if (event.state.toRevert === 'slideFromSettingsView'){
+			loaderApp.settingsView.cancel();
+		}
+	}
 }
 
 BrowserHistoryManager.prototype = {
-    toCreateUrlDialog: function(){
-        window.history.replaceState({'toRevert': 'hideCreateUrlDialog' }, 
-                                    document.title, location.href);
-        window.history.pushState('createUrlView', document.title, location.href);
-    },
-    toUrlSettingsDialog: function(){
-        window.history.replaceState({'toRevert': 'hideUrlSettingsDialog' }, 
-                                    document.title, location.href);
-        window.history.pushState('urlSettings', document.title, location.href);
-    },
-    toSettingsView: function(){
-        window.history.replaceState({'toRevert': 'slideFromSettingsView' }, 
-                                    document.title, location.href);
-        window.history.pushState('settingsView', document.title, location.href);
-    }
+		toCreateUrlDialog: function(){
+			if (!window.history.replaceState)
+				return;
+			window.history.replaceState({'toRevert': 'hideCreateUrlDialog' }, 
+					document.title, location.href);
+			window.history.pushState('createUrlView', document.title, location.href);
+		},
+		toUrlSettingsDialog: function(){
+			if (!window.history.replaceState)
+				return;
+			window.history.replaceState({'toRevert': 'hideUrlSettingsDialog' }, 
+					document.title, location.href);
+			window.history.pushState('urlSettings', document.title, location.href);
+		},
+		toSettingsView: function(){
+	    	if (!window.history.replaceState)
+	    		return;
+			window.history.replaceState({'toRevert': 'slideFromSettingsView' }, 
+					document.title, location.href);
+			window.history.pushState('settingsView', document.title, location.href);
+		}
 }
